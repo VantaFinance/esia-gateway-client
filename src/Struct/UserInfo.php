d23f855678d5db712f62e6b47280fc9092fc52a7
Valentin Nazarov <v.nazarov@pos-credit.ru>
2023-09-27 13:23:01 +0300

    Reorder UserInfo props

--- src/Struct/UserInfo.php
+++ src/Struct/UserInfo.php
@@ -11,17 +11,21 @@ final class UserInfo
 {
     public function __construct(
         public readonly string $uid,
+        public readonly bool $trusted,
+
         public readonly string $firstName,
         public readonly string $middleName,
         public readonly string $lastName,
+        public readonly CountryIso $citizenship,
         public readonly DateTimeImmutable $birthDate,
         public readonly Gender $gender,
-        public readonly bool $trusted,
+
         public readonly string $email,
         public readonly PhoneNumber $mobilePhone,
-        public readonly CountryIso $citizenship,
+
         public readonly SnilsNumber $snils,
         public readonly InnNumber $inn,
+
         public readonly Address $registrationAddress,
         public readonly Address $homeAddress,
     ) {
