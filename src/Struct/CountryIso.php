17934baf7604f93b85c81ed94aee100f4ae88abf
Vlad Shashkov <shashkov.root@gmail.com>
2024-01-20 14:24:23 +0700

    Refactoring struct

--- src/Struct/CountryIso.php
+++ src/Struct/CountryIso.php
@@ -15,21 +15,14 @@ use InvalidArgumentException;
 final class CountryIso
 {
     /**
-     * @var non-empty-string
-     */
-    public readonly string $value;
-
-    /**
      * @param non-empty-string $value
      */
     public function __construct(
-        string $value,
+        public readonly string $value,
     ) {
         if (!preg_match('/^[a-zA-Z]{3}$/', $value)) {
             throw new InvalidArgumentException('Invalid country ISO code, must be an ISO 3166-1 alpha-3 code');
         }
-
-        $this->value = $value;
     }
 
     /**
